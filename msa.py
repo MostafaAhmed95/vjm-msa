6a86c825b6ee6be95958206e0a22f68c34216de6
MostafaAhmed95 <mostafa95.sayed@gmail.com>
2020-02-14 09:14:15 +0300

    spliting the K remain

--- msa.py
+++ msa.py
@@ -5,11 +5,32 @@ import numpy as np
 #we rotate around z_axis
 
 #variables to be set at the first of the code
+E=1
+L=1
+G=1
+S=1
+J=1
+I_x=1
+I_y=1
+I_z=1
+
 q=1
 w_list=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] #19
 
 
 #function for the general K
+K=np.zeros((6,6))
+
+K[0,0]=(E*S)/L
+K[1,1]=(12*E*I_z)/(L**3)
+K[2,2]=(12*E*I_y)/(L**3)
+K[3,3]=(G*J)/(L)
+K[4,4]=(4*E*I_y)/(L)
+K[5,5]=(4*E*I_z)/(L)
+K[1,5]=(-6*E*I_z)/(L**2)
+K[2,4]=(6*E*I_y)/(L**2)
+K[4,2]=(6*E*I_y)/(L**2)
+K[5,5]=(-6*E*I_z)/(L**2)
 
 #function for inverse RR robot
 
@@ -46,11 +67,16 @@ def create_row(l_t):
     b=np.zeros((l_t[0][1].shape[0],114))
     for i in l_t:
         b[:, i[0]*6:(i[0]*6)+6]=i[1]
+    return b
+
+#function for stacking the row
+def mat_stack(x):
+    np.hstack((k_after_reduction,x))
 
 #the boundry conditions
 
 # at base connection
-r_1=[(0,A_r)]
+r_1  = [(0,A_r)]
 r_2=[(6,A_r)]
 r_3=[(12,A_r)]
 
@@ -66,3 +92,9 @@ r_9=[(7,A_r),(8,-A_r)]
 r_10=[(9,A_r),(10,-A_r)]
 r_11=[(13,A_r),(14,-A_r)]
 r_12=[(15,A_r),(16,-A_r)]
+
+#Wrench related equations
+r_13=[(0,A_p),(1,A_p)] #w0
+r_14=[(0,A_p),(1,A_p)] #w1
+
+#
